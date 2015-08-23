5f91855f9fccac16537bcbe26958270b6b859dba
范小豪 <fwh19890125@gmail.com>
2015-08-23 12:45:48 +0800

    add ajax debug code

--- js/index.js
+++ js/index.js
@@ -4,19 +4,51 @@
 		$(".slideBar a").click(function(){
 			//alert($(this).index());
 			//debugger
+			/*
 			index = $(this).index();
 			var old = $("ul.slideImg li.on");
 			old.fadeOut().attr("class", "off");
-			//old.attr("class", "off");
 			$(".slideBar a.on").attr("class","off");
 
 			var li=$(".slideImg li").eq(index);
 			li.fadeIn().attr("class", "on");
-			//li.attr("class", "on");
+			$(this).attr("class","on");*/
+			index = $(this).index();
+			var old = $("ul.slideImg li.on");
+			old.fadeOut(1000, function(){
+				$(this).attr("class", "off");
+				$(".slideImg li").eq(index).fadeIn(500).attr("class", "on");
+			});
+			$(".slideBar a.on").attr("class","off");
 			$(this).attr("class","on");
 
 		});
-		//$("ul.slideImg li.on").fadeOut();
+		$(".signup").click(function(){
+			alert(1);
+			url="/activity/buddaexpress/sign.m"
+			data=$(".m-form").serialize();
+			$.post(url,data,function(response,status){
+				if(status==200){
+					switch(response.code){
+						case 200:
+							alert("success");
+							break;
+						case 401:
+							alert("not login");
+							break;
+						case 203:
+							alert("expire");
+							break;
+						default:
+							alert("error");
+					}
+
+				}
+				else{
+					alert("500");
+				}
+			});
+		});
 	});
 
 })();
